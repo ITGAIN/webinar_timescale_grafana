ce3b3cc1b3fca13aff9aaab0efa5bab42212e8aa
maggusf <35348993+maggusf@users.noreply.github.com>
2024-11-08 08:16:23 +0100

    Add files via upload

---
+++ webinar-timescale-grafana.sql
@@ -0,0 +1,19 @@
+
+CREATE USER grafanareader WITH PASSWORD 'itgain';
+create schema observability;
+create table observability.hist_pg_stat_database as select current_timestamp as time,* from pg_stat_database;
+GRANT USAGE ON SCHEMA observability TO grafanareader;
+GRANT SELECT ON observability.hist_pg_stat_database TO grafanareader;
+
+select * from observability.hist_pg_stat_database
+
+select create_hypertable('observability.hist_pg_stat_database', 'time', migrate_data => TRUE, chunk_time_interval => INTERVAL '1 day', if_not_exists => TRUE);
+alter table observability.hist_pg_stat_database set (timescaledb.compress, timescaledb.compress_orderby = 'time',timescaledb.compress_segmentby = 'datname');
+SELECT add_compression_policy('observability.hist_pg_stat_database', INTERVAL '1h');
+SELECT add_retention_policy('observability.hist_pg_stat_database', INTERVAL '7d');
+
+/* bash
+while true; do docker compose exec -t timescale psql -U postgres -d postgres -c "insert into observability.hist_pg_stat_database select current_timestamp as time,* from pg_stat_database;"; sleep 60;done
+*/
+
+select "time",numbackends from observability.hist_pg_stat_database
